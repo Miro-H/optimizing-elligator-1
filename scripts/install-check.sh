105e12ccfa441e432e7d537efe0718ead389d4d0
Miro Haller <mihaller@student.ethz.ch>
2021-03-08 18:30:09 +0100

    Outsource check installation to a script, do it conditional on already existing installation.

---
+++ scripts/install-check.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#
+# This script installs check 0.15.2 from source on Debian/Ubuntu
+#
+if ! pkg-config --modversion check > /dev/null 2>&1; then
+    echo "Installing Check 0.15.2 from source."
+
+    CHECK_RELEASE=https://github.com/libcheck/check/archive/0.15.2.tar.gz
+
+    apt update
+
+    DEBIAN_FRONTEND=noninteractive apt -y -q \
+        install \
+        make \
+        build-essential \
+        libtool \
+        libsubunit-dev \
+        autoconf \
+        pkg-config \
+        texinfo \
+        wget
+
+    cd /tmp
+    wget $CHECK_RELEASE
+    tar xvf *.tar.gz
+
+    cd check-*
+    autoreconf --install
+    ./configure --prefix=/usr
+    make
+    make check
+    make install
+else
+    echo "Check might already be installed."
+fi
