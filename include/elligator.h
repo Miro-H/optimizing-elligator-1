804c09f2fba77fb67b5e27cfbae10bfa0f79aca1
Christopher Vogelsanger <cvogelsa@student.ethz.ch>
2021-04-24 15:15:49 +0200

    small fixes

--- include/elligator.h
+++ include/elligator.h
@@ -20,9 +20,46 @@ typedef struct Curves // edward curve
     BigInt *r; // r = c + 1/c
 } Curve;
 
+
+static dbl_chunk_size_t chunk_two = 2;
+static dbl_chunk_size_t chunk_three = 3;
+static dbl_chunk_size_t chunk_four = 4;
+static dbl_chunk_size_t chunk_five = 5;
+
+// Special BigInts, never free those! They cannot be copied.
+__attribute__((unused)) static BigInt *big_int_two = &((BigInt) {
+    .sign = 0,
+    .overflow = 0,
+    .size = 1,
+    .alloc_size = 0,
+    .chunks = &chunk_two,
+});
+__attribute__((unused)) static BigInt *big_int_three = &((BigInt) {
+    .sign = 0,
+    .overflow = 0,
+    .size = 1,
+    .alloc_size = 0,
+    .chunks = &chunk_three,
+});
+__attribute__((unused)) static BigInt *big_int_four = &((BigInt) {
+    .sign = 0,
+    .overflow = 0,
+    .size = 1,
+    .alloc_size = 0,
+    .chunks = &chunk_four,
+});
+__attribute__((unused)) static BigInt *big_int_five = &((BigInt) {
+    .sign = 0,
+    .overflow = 0,
+    .size = 1,
+    .alloc_size = 0,
+    .chunks = &chunk_five,
+});
+
+
 CurvePoint elligator_1_string_to_point(BigInt *t, Curve curve);
 
-BigInt * elligator_1_point_to_string(CurvePoint p, Curve curve);
+BigInt* elligator_1_point_to_string(CurvePoint p, Curve curve);
 
 
 #endif // ELLIGATOR_H_
--- src/elligator.c
+++ src/elligator.c
@@ -1,46 +1,6 @@
 #include "elligator.h"
 #include "bigint.h"
 
-// Special BigInts, never free those! They cannot be copied.
-static dbl_chunk_size_t chunk_two = 2;
-static dbl_chunk_size_t chunk_three = 3;
-static dbl_chunk_size_t chunk_four = 4;
-static dbl_chunk_size_t chunk_five = 5;
-
-__attribute__((unused)) static BigInt *big_int_two = &((BigInt) {
-    .sign = 0,
-    .overflow = 0,
-    .size = 1,
-    .alloc_size = 0,
-    .chunks = &chunk_two,
-});
-
-__attribute__((unused)) static BigInt *big_int_three = &((BigInt) {
-    .sign = 0,
-    .overflow = 0,
-    .size = 1,
-    .alloc_size = 0,
-    .chunks = &chunk_three,
-});
-
-__attribute__((unused)) static BigInt *big_int_four = &((BigInt) {
-    .sign = 0,
-    .overflow = 0,
-    .size = 1,
-    .alloc_size = 0,
-    .chunks = &chunk_four,
-});
-
-__attribute__((unused)) static BigInt *big_int_five = &((BigInt) {
-    .sign = 0,
-    .overflow = 0,
-    .size = 1,
-    .alloc_size = 0,
-    .chunks = &chunk_five,
-});
-
-
-
 CurvePoint elligator_1_string_to_point(BigInt *t, Curve curve)
 {
     // TODO: assert t =/= +1, -1
@@ -72,7 +32,7 @@ CurvePoint elligator_1_string_to_point(BigInt *t, Curve curve)
     Y0 = big_int_mul_mod(Y0, Y0, CHIV, curve.q);
 
     BigInt *C2 = big_int_pow(NULL, curve.c, big_int_two, curve.q);
-    C2 = big_int_inverse(C2, C2, curve.q);
+    C2 = big_int_inv(C2, C2, curve.q);
     BigInt *Y3 = big_int_pow(NULL, u, big_int_two, curve.q);
     Y3 = big_int_add(Y3, Y3, C2);
     Y3 = big_int_mod(Y3, Y3, curve.q);
@@ -80,23 +40,23 @@ CurvePoint elligator_1_string_to_point(BigInt *t, Curve curve)
 
     BigInt *Y = big_int_mul_mod(Y0, Y0, Y3, curve.q);  // Y = (χ(v)v)**((q + 1) / 4)χ(v)χ(u**2 + 1 / c**2)
 
-    BigInt *X_plus_1 = big_int_add(NULL, big_int_one, X, curve.q);
+    BigInt *X_plus_1 = big_int_add(NULL, big_int_one, X);
     BigInt *X_plus_1_squared = big_int_pow(NULL, X_plus_1, big_int_two, curve.q);
 
     BigInt *x0 = big_int_sub(NULL, curve.c, big_int_one);
     x0 = big_int_mul_mod(x0, x0, curve.s, curve.q);
     x0 = big_int_mul_mod(x0, x0, X, curve.q);
     x0 = big_int_mul_mod(x0, x0, X_plus_1, curve.q);
-    BigInt *x1 = big_int_inverse(NULL, Y, curve.q);
+    BigInt *x1 = big_int_inv(NULL, Y, curve.q);
     BigInt *x = big_int_mul(NULL, x0, x1); // x = (c − 1)*s*X*(1 + X) / Y
 
     BigInt *rX = big_int_mul_mod(NULL, curve.r, X, curve.q);
-    BigInt *y0 = big_int_sub(NULL, rX, X_plus_1_squared, curve.q);
+    BigInt *y0 = big_int_sub(NULL, rX, X_plus_1_squared);
 
     BigInt *y1 = big_int_add(NULL, rX, X_plus_1_squared);
-    y1 = big_int_inverse(y1, y1, curve.q);
+    y1 = big_int_inv(y1, y1, curve.q);
 
-    BigInt *y = big_int_mul(NULL, y0, y1, curve.q); //  y = (rX − (1 + X)**2) / (rX + (1 + X)**2)
+    BigInt *y = big_int_mul_mod(NULL, y0, y1, curve.q); //  y = (rX − (1 + X)**2) / (rX + (1 + X)**2)
 
     big_int_destroy(u0);
     big_int_destroy(u1);
@@ -130,23 +90,23 @@ CurvePoint elligator_1_string_to_point(BigInt *t, Curve curve)
 
 
 
-BigInt elligator_1_point_to_string(CurvePoint p, Curve curve)
+BigInt* elligator_1_point_to_string(CurvePoint p, Curve curve)
 {
     BigInt *eta0 = big_int_sub(NULL, p.y, big_int_one);
     BigInt *eta1 = big_int_add(NULL, p.y, big_int_one);
     eta1 = big_int_mul_mod(eta1, big_int_two, eta1, curve.q);
-    eta1 = big_int_inverse(eta1, eta1, curve.q);
+    eta1 = big_int_inv(eta1, eta1, curve.q);
     BigInt *eta = big_int_mul_mod(NULL, eta0, eta1, curve.q);  // η = (y-1)/(2(y+1))
 
     BigInt *eta_r = big_int_mul_mod(NULL, eta, curve.r, curve.q);
-    eta_r = big_int_add(eta_r, create_big_int(1), eta_r);
+    eta_r = big_int_add(eta_r, big_int_one, eta_r);
     BigInt *q_1 = big_int_add(NULL, curve.q, big_int_one);
     // TODO: This is module order(q), which is q-1 if q is prime. Is this guaranteed?
-    BigInt *X0 = big_int_sub(NULL, curve.q, big_int_one));
-    X0 = big_int_inverse(X0, big_int_four, X0);
+    BigInt *X0 = big_int_sub(NULL, curve.q, big_int_one);
+    X0 = big_int_inv(X0, big_int_four, X0);
     q_1 = big_int_mul_mod(q_1, q_1, X0, curve.q);
 
-    BigInt *X1 = big_int_pow(NULL, eta_r, create_big_int(2), curve.q);
+    BigInt *X1 = big_int_pow(NULL, eta_r, big_int_two, curve.q);
     X1 = big_int_sub(X1, X1, big_int_one);
     X1 = big_int_pow(X1, X1, q_1, curve.q);
     X1 = big_int_sub(X1, X1, eta_r);
@@ -160,31 +120,31 @@ BigInt elligator_1_point_to_string(CurvePoint p, Curve curve)
     x0 = big_int_mul_mod(x0, x0, x1, curve.q);
     BigInt *x2 = big_int_chi(NULL, curve.c, curve.q);
     x0 = big_int_mul_mod(x0, x0, x2, curve.q);
-    BigInt *x3 = big_int_inverse(NULL, curve.r, curve.q);
+    BigInt *x3 = big_int_inv(NULL, curve.r, curve.q);
     BigInt *x = big_int_mul_mod(NULL, x0, x3, curve.q); // x = 2s(c − 1)χ(c)/r
 
     BigInt *z0 = big_int_sub(NULL, curve.c, big_int_one);
     z0 = big_int_mul_mod(z0, z0, curve.s, curve.q);
     z0 = big_int_mul_mod(z0, z0, X, curve.q);
-    BigInt *z1 = big_int_add(create_big_int(1), X);
+    BigInt *z1 = big_int_add(NULL, big_int_one, X);
     z0 = big_int_mul_mod(z0, z0, z1, curve.q);
     z0 = big_int_mul_mod(z0, z0, x, curve.q);
     BigInt *z2 = big_int_pow(NULL, X, big_int_two, curve.q);
     BigInt *z3 = big_int_pow(NULL, curve.c, big_int_two, curve.q);
-    z3 = big_int_inverse(z3, z3, curve.q);
+    z3 = big_int_inv(z3, z3, curve.q);
     z2 = big_int_add(z2, z2, z3);
     z0 = big_int_mul_mod(z0, z0, z2, curve.q);
     z0 = big_int_chi(z0, z0, curve.q);
     BigInt *z = big_int_mod(NULL, z0, curve.q);  // z = χ((c − 1)sX(1 + X)x(X**2 + 1/c**2))
 
-    BigInt *u = big_int_mul_mod(NULL, z, X), curve.q; // u = zX
+    BigInt *u = big_int_mul_mod(NULL, z, X, curve.q); // u = zX
 
     big_int_destroy(u);
     
     BigInt *t0 = big_int_sub(NULL, big_int_one, u);
-    BigInt *t1 = big_int_add(NULL, big_int_one,, u);
-    t1 = big_int_inverse(t1, t1, curve.q);
-    BigInt t = big_int_mul_mod(NULL, t0, t1, curve.q); // t = (1 − u)/(1 + u)
+    BigInt *t1 = big_int_add(NULL, big_int_one, u);
+    t1 = big_int_inv(t1, t1, curve.q);
+    BigInt *t = big_int_mul_mod(NULL, t0, t1, curve.q); // t = (1 − u)/(1 + u)
     
     big_int_destroy(eta0);
     big_int_destroy(eta1);
@@ -211,7 +171,7 @@ BigInt elligator_1_point_to_string(CurvePoint p, Curve curve)
     big_int_destroy(t0);
     big_int_destroy(t1);
 
-     return t;
+    return t;
 }
 
 
