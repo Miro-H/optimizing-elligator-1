06a6a67340b25cf5efe83a050f7bfc1110be0e97
Miro Haller <mihaller@student.ethz.ch>
2021-05-04 01:47:43 +0200

    Enable publish plots for gen_runtime_plots.py

--- timing/scripts/make_all_plots.sh
+++ timing/scripts/make_all_plots.sh
@@ -19,7 +19,10 @@ PLOTS_DIR=${TIMING_BASE_DIR}/plots
 if [[ -z "${PUBLISH}" ]]; then
     PLOTS_DIR=${PLOTS_DIR}/local
 fi
-${SCRIPTS_DIR}/gen_runtime_plots.py                         
+
+${SCRIPTS_DIR}/gen_runtime_plots.py                     \
+    --plots_dir "${PLOTS_DIR}"                          \
+    --logs_dir "${LOGS_DIR}"                           \
 
 ################################################################################
 #                  Generate bar plots of runtime measurements                  #
