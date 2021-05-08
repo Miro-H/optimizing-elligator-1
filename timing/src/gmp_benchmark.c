bccffb7c340401831770d7adcff6c85d8eab3a28
Freya Murphy <fmurphy@student.ethz.ch>
2021-05-08 13:51:35 +0200

    Created benchmarking file

---
+++ timing/src/gmp_benchmark.c
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the ASL project "Censorship-avoiding high-speed EC
+ * (Elligator with Curve1174)"
+ * (https://gitlab.inf.ethz.ch/COURSE-ASL/asl21/team36).
+ *
+ * Short description of this file:
+ * This file implements benchmarks of the GNU MP Bignum Library.
+ */
+
+/*
+ * Includes
+ */
+#include <gmp.h>
+
+// Include header files
+#include "benchmark_helpers.h"
+
+#ifndef LOG_PATH
+    #define LOG_PATH "../logs"
+#endif
+
