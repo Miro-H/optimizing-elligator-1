066a2a5b5c24e1672a3244ea0aecafcd63af37e3
Miro Haller <mihaller@student.ethz.ch>
2021-05-13 12:22:53 +0200

    Add memcpy benchmark (untested)

---
+++ timing/include/memcpy_benchmark.h
@@ -0,0 +1,27 @@
+#ifndef MEMCPY_BENCHMARK_H_ /* Include guard */
+#define MEMCPY_BENCHMARK_H_
+
+#ifndef LOG_PATH
+#define LOG_PATH "../logs/runtime"
+#endif
+
+#ifndef BIGINTSIZE
+// 256 bits
+#define BIGINTSIZE 8
+#endif
+
+#ifndef SETS
+#define SETS 10
+#endif
+
+#ifndef REPS
+#define REPS 10000
+#endif
+
+/*
+ * Global variables
+ */
+void **mem_array;
+int64_t mem_elem_size;
+
+#endif // MEMCPY_BENCHMARK_H_
---
+++ timing/src/memcpy_benchmark.c
@@ -0,0 +1,76 @@
+/*
+ * Includes
+ */
+#include <stdlib.h>
+
+// Include header files
+#include "memcpy_benchmark.h"
+#include "debug.h"
+#include "benchmark_helpers.h"
+
+void bench_memcpy_prep(void *argptr)
+{
+    int64_t elem_size = ((int64_t *)argptr)[0];
+    int64_t array_size = ((int64_t *)argptr)[1];
+    mem_array = (void **) malloc(array_size * elem_size);
+}
+
+void bench_memcpy_prep(void *argptr)
+{
+    int64_t elem_size = ((int64_t *)argptr)[0];
+    int64_t array_size = ((int64_t *)argptr)[1];
+    mem_array = (void **) malloc(array_size * elem_size);
+    mem_elem_size = elem_size;
+}
+
+void bench_memcpy_fn(void *argptr)
+{
+    int64_t i = *((int64_t *) arg);
+    mem_array[i] = (void *) malloc(mem_elem_size);
+}
+
+void bench_memcpy_cleanup(void *argptr)
+{
+    int64_t used_values = ((int64_t *) argptr)[0];
+
+    for (int64_t i = 0; i < used_values; i++)
+    {
+        free(mem_array[i]);
+    }
+    free(mem_array);
+}
+
+int main(int argc, char const *argv[])
+{
+    int64_t bench_memcpy_32_args[] = {
+        72, // 17 * 4 byte chunks, 4 bytes metadata
+        REPS
+    };
+
+    BenchmarkClosure bench_memcpy_32_closure = {
+        .bench_prep_args = bench_memcpy_32_args,
+        .bench_prep_fn = bench_memcpy_prep,
+        .bench_fn = bench_memcpy_fn,
+        .bench_cleanup_fn = bench_memcpy_cleanup,
+    };
+    benchmark_runner(bench_memcpy_32_closure, "memcpy 32-bit chunks", path,
+        SETS, REPS, REPS);
+
+
+    int64_t bench_memcpy_64_args[] = {
+        144, // 17 * 8 byte chunks, 4 bytes metadata, 4 bytes padding
+        REPS,
+    };
+
+    BenchmarkClosure bench_memcpy_64_closure = {
+        .bench_prep_args = bench_memcpy_64_args,
+        .bench_prep_fn = bench_memcpy_prep,
+        .bench_fn = bench_memcpy_fn,
+        .bench_cleanup_fn = bench_memcpy_cleanup,
+    };
+
+    benchmark_runner(bench_memcpy_64_closure, "memcpy 64-bit chunks", path,
+        SETS, REPS, REPS);
+
+    return EXIT_SUCCESS;
+}
