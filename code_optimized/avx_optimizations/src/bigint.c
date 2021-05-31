e2568d07e955f14a5b6956e0974fe783c9c6c7a8
Freya Murphy <fmurphy@student.ethz.ch>
2021-05-31 14:10:07 +0200

    created mulx4 function

--- code_optimized/avx_optimizations/src/bigint.c
+++ code_optimized/avx_optimizations/src/bigint.c
@@ -1968,6 +1968,130 @@ BigInt *big_int_square_32(BigInt *r, BigInt *a)
 // === === === === === === === === === === === === === === === === === === ===
 
 /**
+ * \brief Calculate r0 = a0 * b0, r1 = a1 * b1, r2 = a2 * b2, r3 = a3 * b3
+ *
+ * \assumption r0, r1, r2, r3, a0, a1, a2, a3, b0, b1, b2, b3 != NULL
+ * \assumption a0->size + b0->size <= BIGINT_FIXED_SIZE_INTERNAL
+ * \assumption a1->size + b1->size <= BIGINT_FIXED_SIZE_INTERNAL
+ * \assumption a2->size + b2->size <= BIGINT_FIXED_SIZE_INTERNAL
+ * \assumption a3->size + b3->size <= BIGINT_FIXED_SIZE_INTERNAL
+ * \assumption a0 != b0, a1 != b1, etc. i.e., no aliasing
+ * \assumption a0->size = a1->size = a2->size = a3->size
+ * \assumption b1->size = b1->size = b2->size = b3->size
+ */
+BigInt *big_int_mul_4(BigInt *r0, BigInt *r1, BigInt *r2, BigInt *r3,
+                      BigInt *a0, BigInt *a1, BigInt *a2, BigInt *a3,
+                      BigInt *b0, BigInt *b1, BigInt *b2, BigInt *b3)
+{
+    ADD_STAT_COLLECTION(BIGINT_TYPE_BIG_INT_MUL_4);
+
+    int64_t i, j;
+    dbl_chunk_size_t carry0;
+    dbl_chunk_size_t carry1;
+    dbl_chunk_size_t carry2;
+    dbl_chunk_size_t carry3;
+
+    *r0 = (BigInt) {0};
+    *r1 = (BigInt) {0};
+    *r2 = (BigInt) {0};
+    *r3 = (BigInt) {0};
+
+    r0->sign = a0->sign ^ b0->sign;
+    r1->sign = a1->sign ^ b1->sign;
+    r2->sign = a2->sign ^ b2->sign;
+    r3->sign = a3->sign ^ b3->sign;
+
+    r0->size = a0->size + b0->size; ADD_STAT_COLLECTION(BASIC_ADD_SIZE)
+    r1->size = a1->size + b1->size; ADD_STAT_COLLECTION(BASIC_ADD_SIZE)
+    r2->size = a2->size + b2->size; ADD_STAT_COLLECTION(BASIC_ADD_SIZE)
+    r3->size = a3->size + b3->size; ADD_STAT_COLLECTION(BASIC_ADD_SIZE)
+
+    for (i = 0; i < b0->size; ++i) { ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+        // shortcut for zero chunk
+        if (b0->chunks[i] == 0) {
+            r0->chunks[i + a0->size] = 0; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+        }
+        if (b1->chunks[i] == 0) {
+            r1->chunks[i + a1->size] = 0; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+        }
+        if (b2->chunks[i] == 0)
+        {
+            r2->chunks[i + a2->size] = 0; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+        }
+        if (b3->chunks[i] == 0)
+        {
+            r3->chunks[i + a3->size] = 0; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+        }
+        else {
+            // Multiply and add chunks
+            carry0 = 0;
+            carry1 = 0;
+            carry2 = 0;
+            carry3 = 0;
+            for (j = 0; j < a0->size; ++j) { ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+                carry0 += a0->chunks[j] * b0->chunks[i] + r0->chunks[i + j];
+                carry1 += a1->chunks[j] * b1->chunks[i] + r1->chunks[i + j];
+                carry2 += a2->chunks[j] * b2->chunks[i] + r2->chunks[i + j];
+                carry3 += a3->chunks[j] * b3->chunks[i] + r3->chunks[i + j];
+
+                ADD_STAT_COLLECTION(BASIC_ADD_CHUNK) ADD_STAT_COLLECTION(BASIC_ADD_CHUNK) 
+                ADD_STAT_COLLECTION(BASIC_ADD_OTHER) ADD_STAT_COLLECTION(BASIC_MUL_CHUNK)
+                ADD_STAT_COLLECTION(BASIC_ADD_CHUNK) ADD_STAT_COLLECTION(BASIC_ADD_CHUNK) 
+                ADD_STAT_COLLECTION(BASIC_ADD_OTHER) ADD_STAT_COLLECTION(BASIC_MUL_CHUNK)
+                ADD_STAT_COLLECTION(BASIC_ADD_CHUNK) ADD_STAT_COLLECTION(BASIC_ADD_CHUNK) 
+                ADD_STAT_COLLECTION(BASIC_ADD_OTHER) ADD_STAT_COLLECTION(BASIC_MUL_CHUNK)
+                ADD_STAT_COLLECTION(BASIC_ADD_CHUNK) ADD_STAT_COLLECTION(BASIC_ADD_CHUNK) 
+                ADD_STAT_COLLECTION(BASIC_ADD_OTHER) ADD_STAT_COLLECTION(BASIC_MUL_CHUNK)
+
+                r0->chunks[i + j] = carry0 & BIGINT_RADIX_FOR_MOD; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+                r1->chunks[i + j] = carry1 & BIGINT_RADIX_FOR_MOD; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+                r2->chunks[i + j] = carry2 & BIGINT_RADIX_FOR_MOD; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+                r3->chunks[i + j] = carry3 & BIGINT_RADIX_FOR_MOD; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+
+                carry0 >>= BIGINT_CHUNK_BIT_SIZE;
+                carry1 >>= BIGINT_CHUNK_BIT_SIZE;
+                carry2 >>= BIGINT_CHUNK_BIT_SIZE;
+                carry3 >>= BIGINT_CHUNK_BIT_SIZE;
+
+                r0->chunks[i + a0->size] = carry0; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+                r1->chunks[i + a1->size] = carry1; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+                r2->chunks[i + a2->size] = carry2; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+                r3->chunks[i + a3->size] = carry3; ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+            }
+        }
+    }
+
+    // Prune leading zeros
+    for (int64_t i = r0->size - 1; i > 0; --i) { ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+        if (r0->chunks[i])
+            break;
+        r0->size--; ADD_STAT_COLLECTION(BASIC_ADD_SIZE)
+    }
+
+    for (int64_t i = r1->size - 1; i > 0; --i) { ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+        if (r1->chunks[i])
+            break;
+        r1->size--; ADD_STAT_COLLECTION(BASIC_ADD_SIZE)
+    }
+
+    for (int64_t i = r2->size - 1; i > 0; --i) { ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+        if (r2->chunks[i])
+            break;
+        r2->size--; ADD_STAT_COLLECTION(BASIC_ADD_SIZE)
+    }
+
+    for (int64_t i = r3->size - 1; i > 0; --i) { ADD_STAT_COLLECTION(BASIC_ADD_OTHER)
+        if (r3->chunks[i])
+            break;
+        r3->size--; ADD_STAT_COLLECTION(BASIC_ADD_SIZE)
+    }
+
+    return r0;
+}
+
+// === === === === === === === === === === === === === === === === === === ===
+
+/**
  * \brief Calculate r = a * b
  *
  * \assumption r, a, b != NULL
