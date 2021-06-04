f78d7aaf784dbfd0a7f61c790dec578efcdf1a5d
Freya Murphy <fmurphy@student.ethz.ch>
2021-06-04 21:24:41 +0200

    All gmp_elligator tests passing

--- code_unoptimized/src/gmp_elligator.c
+++ code_unoptimized/src/gmp_elligator.c
@@ -182,24 +182,39 @@ GMP_CurvePoint *gmp_elligator_1_string_to_point(GMP_CurvePoint *r, mpz_t t, GMP_
           Y, Y0, Y1, Y2, Y3, C2,
           x, x0, rX,
           y, y0, y1,
-          gmp_two;
+          inverse;
 
-    mpz_init(gmp_two);
     mpz_init(q_half);
-    mpz_init(x);
-    mpz_init(y);
+    mpz_init(u);
     mpz_init(u0);
     mpz_init(u1);
-    mpz_init(u);
+    mpz_init(v);
     mpz_init(v0);
-
-    mpz_set_si(gmp_two, 2);
+    mpz_init(v1);
+    mpz_init(v2);
+    mpz_init(CHIV);
+    mpz_init(X);
+    mpz_init(X_plus_1);
+    mpz_init(X_plus_1_squared);
+    mpz_init(Y);
+    mpz_init(Y0);
+    mpz_init(Y1);
+    mpz_init(Y2);
+    mpz_init(Y3);
+    mpz_init(C2);
+    mpz_init(x);
+    mpz_init(x0);
+    mpz_init(rX);
+    mpz_init(y);
+    mpz_init(y0);
+    mpz_init(y1);
+    mpz_init(inverse);
 
     // Enforce correct input range
     //q_half = big_int_sub(NULL, curve.q, big_int_one);
     mpz_sub_ui(q_half, curve.q, 1);
     //big_int_div(q_half, q_half, big_int_two);
-    mpz_fdiv_q(q_half, q_half, gmp_two);
+    mpz_fdiv_q_ui(q_half, q_half, 2);
 
     //if (big_int_compare(t, big_int_zero) == -1 || big_int_compare(t, q_half) == 1)
     if (mpz_cmp_si(t, 0) == -1 || mpz_cmp(t, q_half) == 1)
@@ -215,37 +230,46 @@ GMP_CurvePoint *gmp_elligator_1_string_to_point(GMP_CurvePoint *r, mpz_t t, GMP_
     }
     else {
         //u0 = big_int_sub(NULL, big_int_one, t);
-        mpz_ui_sub(u0, 1, t);
+        mpz_ui_sub(u0, 1, t); // u0 = -6
         //u1 = big_int_add(NULL, big_int_one, t);
-        mpz_add_ui(u1, t, 1);
+        mpz_add_ui(u1, t, 1); // u1 = 8
         //u = big_int_div_mod(NULL, u0, u1, curve.q); // u = (1 − t) / (1 + t)
-        mpz_fdiv_q(u, u0, u1);
+        //mpz_fdiv_q(u, u0, u1);
+        //mpz_mod(u, u, curve.q);
+        mpz_invert(inverse, u1, curve.q);
+
+        mpz_mul(u, u0, inverse);
+        //-0x5fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffa
         mpz_mod(u, u, curve.q);
+        //0x1fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffd
 
         //v0 = big_int_pow(NULL, u, big_int_five, curve.q);
         mpz_powm_ui(v0, u, 5, curve.q);
+        // 0x7c9fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7
         //v1 = big_int_pow(NULL, curve.r, big_int_two, curve.q);
         mpz_powm_ui(v1, curve.r, 2, curve.q);
+        // 0x1c9c4399a2b9d9af7aa044b36ab903eb9e91e21c901e4a392e6e34834a14bea
         //v1 = big_int_sub(v1, v1, big_int_two);
         mpz_sub_ui(v1, v1, 2);
+        // 1c9c4399a2b9d9af7aa044b36ab903eb9e91e21c901e4a392e6e34834a14be8
         //v2 = big_int_pow(NULL, u, big_int_three, curve.q);
-        mpz_powm_ui(v2, u, 3, curve.q);
+        mpz_powm_ui(v2, u, 3, curve.q); // 0x79ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7
         //v1 = big_int_mul_mod(v1, v1, v2, curve.q);
         mpz_mul(v1, v1, v2);
-        mpz_mod(v1, v1, curve.q);
+        mpz_mod(v1, v1, curve.q); // 0x3ee137b2f599829f84463044ef9f258991a749bf33338afe06981d89cbf3f9
         //v0 = big_int_add(v0, v0, v1);
-        mpz_add(v0, v0, v1);
+        mpz_add(v0, v0, v1); // 0x808e137b2f599829f84463044ef9f258991a749bf33338afe06981d89cbf3f0
         //v0 = big_int_add(v0, v0, u);
-        mpz_add(v0, v0, u);
+        mpz_add(v0, v0, u); // 0xa08e137b2f599829f84463044ef9f258991a749bf33338afe06981d89cbf3ed
         //v = big_int_mod(NULL, v0, curve.q);  // v = u**5 + (r**2 − 2)*u**3 + u
-        mpz_mod(v, v0, curve.q);
+        mpz_mod(v, v0, curve.q); // 0x208e137b2f599829f84463044ef9f258991a749bf33338afe06981d89cbf3f6
 
         //CHIV = big_int_chi(NULL, v, curve.q);
-        mpz_chi(CHIV, v, curve.q);
+        mpz_chi(CHIV, v, curve.q); // 1
 
         //X = big_int_mul_mod(NULL, CHIV, u, curve.q);  // X = χ(v)u
-        mpz_mul(X, CHIV, u);
-        mpz_mul(X, X, curve.q);
+        mpz_mul(X, CHIV, u); // 0x1fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffd
+        mpz_mod(X, X, curve.q);
 
         //Y0 = big_int_mul_mod(NULL, CHIV, v, curve.q);
         mpz_mul(Y0, CHIV, v);
@@ -275,6 +299,7 @@ GMP_CurvePoint *gmp_elligator_1_string_to_point(GMP_CurvePoint *r, mpz_t t, GMP_
         //Y = big_int_mul_mod(Y0, Y0, Y3, curve.q);  // Y = (χ(v)v)**((q + 1) / 4)χ(v)χ(u**2 + 1 / c**2)
         mpz_mul(Y, Y0, Y3);
         mpz_mod(Y, Y, curve.q);
+        //mpz_set(Y0, Y);
 
         //X_plus_1 = big_int_add(NULL, big_int_one, X);
         mpz_add_ui(X_plus_1, X, 1);
@@ -293,7 +318,10 @@ GMP_CurvePoint *gmp_elligator_1_string_to_point(GMP_CurvePoint *r, mpz_t t, GMP_
         mpz_mul(x0, x0, X_plus_1);
         mpz_mod(x0, x0, curve.q);
         //x = big_int_div_mod(NULL, x0, Y, curve.q); // x = (c − 1)*s*X*(1 + X) / Y
-        mpz_fdiv_q(x, x0, Y);
+        //mpz_fdiv_q(x, x0, Y);
+        //mpz_mod(x, x, curve.q);
+        mpz_invert(inverse, Y, curve.q);
+        mpz_mul(x, x0, inverse);
         mpz_mod(x, x, curve.q);
 
         //rX = big_int_mul_mod(NULL, curve.r, X, curve.q);
@@ -305,7 +333,10 @@ GMP_CurvePoint *gmp_elligator_1_string_to_point(GMP_CurvePoint *r, mpz_t t, GMP_
         //y1 = big_int_add(NULL, rX, X_plus_1_squared);
         mpz_add(y1, rX, X_plus_1_squared);
         //y = big_int_div_mod(NULL, y0, y1, curve.q); //  y = (rX − (1 + X)**2) / (rX + (1 + X)**2)
-        mpz_fdiv_q(y, y0, y1);
+        //mpz_fdiv_q(y, y0, y1);
+        //mpz_mod(y, y, curve.q);
+        mpz_invert(inverse, y1, curve.q);
+        mpz_mul(y, y0, inverse);
         mpz_mod(y, y, curve.q);
 
         //big_int_destroy(u0);
@@ -377,7 +408,7 @@ GMP_CurvePoint *gmp_elligator_1_string_to_point(GMP_CurvePoint *r, mpz_t t, GMP_
  * \param p Point on the given curve (with x, y coordinates)
  * \param curve Curve satisfying the properties needed for Elligator one (e.g. Curve1174)
  */
-void elligator_1_point_to_string(mpz_t t, GMP_CurvePoint p, GMP_Curve curve)
+void gmp_elligator_1_point_to_string(mpz_t t, GMP_CurvePoint p, GMP_Curve curve)
 {
     //BigInt *eta, *eta0, *eta1,
            //*eta_r, *q_exp,
@@ -392,7 +423,7 @@ void elligator_1_point_to_string(mpz_t t, GMP_CurvePoint p, GMP_Curve curve)
             z, z0, z1, z2, z3,
             u,
             t0, t1,
-            q_half;
+            q_half, inverse;
 
     mpz_init(eta);
     mpz_init(eta0);
@@ -410,86 +441,93 @@ void elligator_1_point_to_string(mpz_t t, GMP_CurvePoint p, GMP_Curve curve)
     mpz_init(t0);
     mpz_init(t1);
     mpz_init(q_half);
+    mpz_init(inverse);
 
     //eta0 = big_int_sub(NULL, p.y, big_int_one);
-    mpz_sub_ui(eta0, p.y, 1);
+    mpz_sub_ui(eta0, p.y, 1); // 0x61c507d563235bea439c1d53bddf33e0d87c0041d3228cd0ca00c3d2a59025d
+
     //eta1 = big_int_add(NULL, p.y, big_int_one);
-    mpz_add_ui(eta1, p.y, 1);
+    mpz_add_ui(eta1, p.y, 1); // 0x61c507d563235bea439c1d53bddf33e0d87c0041d3228cd0ca00c3d2a59025f
     //eta1 = big_int_mul_mod(eta1, big_int_two, eta1, curve.q);
     mpz_mul_ui(eta1, eta1, 2);
-    mpz_mod(eta1, eta1, curve.q);
+    mpz_mod(eta1, eta1, curve.q); // 0x438a0faac646b7d487383aa77bbe67c1b0f80083a64519a1940187a54b204c7
     //eta = big_int_div_mod(eta0, eta0, eta1, curve.q);  // η = (y-1)/(2(y+1))
-    mpz_div(eta0, eta0, eta1);
-    mpz_mod(eta, eta0, curve.q); 
+    //mpz_div(eta0, eta0, eta1);
+    mpz_invert(inverse, eta1, curve.q); // 0xfdeaf0cc622f4ccb7e58f8b65ba2c8cd93fa3d716cfc203442904d8c74ae33
+    mpz_mul(eta0, eta0, inverse); // 0x60f972cdd1e058a5053e34a2b41d9b0c7934583bf93dfd9c59f1611cfa712b332a00bee79db2acea4ee5b229ea58784525d50e59fabea9927e690dd7aae87
+    mpz_mod(eta, eta0, curve.q); // 0x2042a1e673ba16669034e0e9348ba6e64d80b851d2607bf977adf64e716a396
+    mpz_set(eta0, eta); // 0x2042a1e673ba16669034e0e9348ba6e64d80b851d2607bf977adf64e716a396
 
     //eta_r = big_int_mul_mod(NULL, eta, curve.r, curve.q);
-    mpz_mul(eta_r, eta, curve.r);
-    mpz_mod(eta_r, eta_r, curve.q);
+    mpz_mul(eta_r, eta, curve.r); 
+    mpz_mod(eta_r, eta_r, curve.q); // 0x65c0696e9286c9ad802ed257bd2290c769ade2be373d22dd6ab4db2c27ae52
     //eta_r = big_int_add(eta_r, big_int_one, eta_r);
-    mpz_add_ui(eta_r, eta_r, 1);
+    mpz_add_ui(eta_r, eta_r, 1); // 0x65c0696e9286c9ad802ed257bd2290c769ade2be373d22dd6ab4db2c27ae53
     //q_exp = big_int_add(NULL, curve.q, big_int_one);
-    mpz_add_ui(q_exp, curve.q, 1);
+    mpz_add_ui(q_exp, curve.q, 1); // 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff8
     //q_exp = big_int_div(q_exp, q_exp, big_int_four);
-    mpz_fdiv_q_ui(q_exp, q_exp, 4);
+    mpz_fdiv_q_ui(q_exp, q_exp, 4); // 0x1fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe
 
     //X0 = big_int_pow(NULL, eta_r, big_int_two, curve.q);
-    mpz_powm_ui(X0, eta_r, 2, curve.q);
+    mpz_powm_ui(X0, eta_r, 2, curve.q); // 0x7c31bb9461892aa81d069bb04dbf0cc99b0dcf181363c8df4e0c3775ed44a75
     //X0 = big_int_sub(X0, X0, big_int_one);
-    mpz_sub_ui(X0, X0, 1);
+    mpz_sub_ui(X0, X0, 1); // 0x7c31bb9461892aa81d069bb04dbf0cc99b0dcf181363c8df4e0c3775ed44a74
     //X0 = big_int_pow(X0, X0, q_exp, curve.q);
-    mpz_powm(X0, X0, q_exp, curve.q);
+    mpz_powm(X0, X0, q_exp, curve.q); // 0xe7c5f9fd8513685b7d93265780e4a332738ffd4100dda9b29dee72a745abfd
     //X = big_int_sub_mod(X0, X0, eta_r, curve.q); // X = −(1 + ηr) + ((1 + ηr)**2 − 1)**((q+1)/4)
     mpz_sub(X0, X0, eta_r);
-    mpz_mod(X, X0, curve.q);
+    mpz_mod(X, X0, curve.q); // 0x8205908ef28c9eadfd6453ffc3c2126b09e21a82c9a086d53339977b1dfdaa
 
     //z0 = big_int_sub(NULL, curve.c, big_int_one);
-    mpz_sub_ui(z0, curve.c, 1);
+    mpz_sub_ui(z0, curve.c, 1); // 0x4d1a3398ed42ceeb451d20824ca9cb49b69ef546bd7e6546aef19af1f9e49e0
     //z0 = big_int_mul_mod(z0, z0, curve.s, curve.q);
     mpz_mul(z0, z0, curve.s);
-    mpz_mod(z0, z0, curve.q);
+    mpz_mod(z0, z0, curve.q); // 0x67897dc1c0ccc95f80ac25cfb7dba3f085e0a97c32385ba7dc4079961d335b1
     //z0 = big_int_mul_mod(z0, z0, X, curve.q);
-    mpz_mod(z0, z0, X);
-    mpz_mod(z0, z0, curve.q);
+    mpz_mul(z0, z0, X);
+    mpz_mod(z0, z0, curve.q); // 0x7289c8e3c691b2cdeda79b22f626bc14be9185783eae03fc78150da1a82b385
     //z1 = big_int_add(NULL, big_int_one, X);
-    mpz_add_ui(z1, X, 1);
+    mpz_add_ui(z1, X, 1); // 0x8205908ef28c9eadfd6453ffc3c2126b09e21a82c9a086d53339977b1dfdab
     //z0 = big_int_mul_mod(z0, z0, z1, curve.q);
     mpz_mul(z0, z0, z1);
-    mpz_mod(z0, z0, curve.q);
+    mpz_mod(z0, z0, curve.q); // 0x7db431c5e5c193393bdd96c365cbb33fcc58461ad09a455ea95c93ee8910db
     //z0 = big_int_mul_mod(z0, z0, p.x, curve.q);
     mpz_mul(z0, z0, p.x);
-    mpz_mod(z0, z0, curve.q);
+    mpz_mod(z0, z0, curve.q); // 0x66f54486f406cc911e388dbabb7db5567e7705897f35f26cbdc7995b02a5bbe
     //z2 = big_int_pow(NULL, X, big_int_two, curve.q);
-    mpz_powm_ui(z2, X, 2, curve.q);
+    mpz_powm_ui(z2, X, 2, curve.q); // 0x461594094bf365f210b33449a23391b6b3e9a3dfcdf2c4630b828a4dcb5283c
     //z3 = big_int_pow(NULL, curve.c, big_int_two, curve.q);
-    mpz_powm_ui(z3, curve.c, 2, curve.q);
+    mpz_powm_ui(z3, curve.c, 2, curve.q); // 0x257d2aeaca86b7a445ef47057ad0c8cf7a16154ab8860fa4f5ba21af86a4b25
     //z3 = big_int_inv(z3, z3, curve.q);
-    mpz_invert(z3, z3, curve.q);
+    mpz_invert(z3, z3, curve.q); // 0x771f18aed833220b34b0fdadefe83b1c247bccd1d7983a9438b412d3c3700ba
     //z2 = big_int_add(z2, z2, z3);
-    mpz_add(z2, z2, z3);
+    mpz_add(z2, z2, z3); // 0xbd34acb8242687fd456431f7921bccd2d86570b1a58afef744369d218ec28f6
     //z0 = big_int_mul_mod(z0, z0, z2, curve.q);
     mpz_mul(z0, z0, z2);
-    mpz_mod(z0, z0, curve.q);
+    mpz_mod(z0, z0, curve.q); // 0x55f6286c0e9d7eddef448397c92b715fe48af231dd21f62cbf14f11fd7d932
     //z0 = big_int_chi(z0, z0, curve.q);
-    mpz_chi(z0, z0, curve.q);
+    mpz_chi(z0, z0, curve.q); // 1
     //z = big_int_mod(z0, z0, curve.q);  // z = χ((c − 1)sX(1 + X)x(X**2 + 1/c**2))
-    mpz_mod(z, z0, curve.q);
+    mpz_mod(z, z0, curve.q); // 1
 
     //u = big_int_mul_mod(NULL, z, X, curve.q); // u = zX
     mpz_mul(u, z, X);
-    mpz_mod(u, u, curve.q);
+    mpz_mod(u, u, curve.q); // 0x8205908ef28c9eadfd6453ffc3c2126b09e21a82c9a086d53339977b1dfdaa
 
     //t0 = big_int_sub(NULL, big_int_one, u);
-    mpz_ui_sub(t0, 1, u);
+    mpz_ui_sub(t0, 1, u); // -0x8205908ef28c9eadfd6453ffc3c2126b09e21a82c9a086d53339977b1dfda9
     //t1 = big_int_add(NULL, big_int_one, u);
-    mpz_add_ui(t1, u, 1);
+    mpz_add_ui(t1, u, 1); // 0x8205908ef28c9eadfd6453ffc3c2126b09e21a82c9a086d53339977b1dfdab
     //t = big_int_div_mod(t, t0, t1, curve.q); // t = (1 − u)/(1 + u)
-    mpz_fdiv_q(t, t0, t1);
-    mpz_mod(t, t, curve.q);
+    //mpz_fdiv_q(t, t0, t1);
+    mpz_invert(inverse, t1, curve.q); // 0x55e6f7891a2b3c4ccc3b2a190d5e55e6f7ef8
+    mpz_mul(t, t0, inverse);
+    mpz_mod(t, t, curve.q); // 0xabcdef1234567899987654321abcabcdefdef
 
     //q_half = big_int_sub(NULL, curve.q, big_int_one);
-    mpz_sub_ui(q_half, curve.q, 1);
+    mpz_sub_ui(q_half, curve.q, 1); // 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff6
     //q_half = big_int_div(q_half, q_half, big_int_two); // q_half = (q - 1)/2
-    mpz_fdiv_q_ui(q_half, q_half, 2);
+    mpz_fdiv_q_ui(q_half, q_half, 2); // 0x3fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffb
 
     //if (big_int_compare(t, q_half) == 1) {
     if (mpz_cmp(t, q_half) == 1) {
@@ -528,6 +566,7 @@ void elligator_1_point_to_string(mpz_t t, GMP_CurvePoint p, GMP_Curve curve)
     mpz_clear(t0);
     //big_int_destroy(t1);
     mpz_clear(t1);
+    mpz_clear(inverse);
 
     //return &t;
 }
--- tests/src/gmp_elligator_test.c
+++ tests/src/gmp_elligator_test.c
@@ -248,14 +248,285 @@ END_TEST
  */
 START_TEST(test_e2e)
 {
+    GMP_Curve curve;
+    GMP_CurvePoint curve_point;
+    mpz_init(curve_point.x);
+    mpz_init(curve_point.y);
 
+    mpz_t r, t, x, y;
+
+    mpz_init(r);
+    mpz_init(t);
+    mpz_init(x);
+    mpz_init(y);
+
+    gmp_init_curve1174(&curve);
+
+    mpz_set_si(t, 7);
+    mpz_set_str(x,
+        "AB65983CF55A18C0E2C8BB8A156E030566D23767D6C1473ACFCF4D17439AC7",
+        16);
+    mpz_set_str(y,
+        "49C01F8D8C86ECB362B3952FA93ABD8CF512B09225BCEE9E76BC5E0C9A6E17E",
+        16);
+
+    // Map BigInt to curve point
+    gmp_elligator_1_string_to_point(&curve_point, t, curve);
+    ck_assert_int_eq(mpz_cmp(curve_point.x, x), 0);
+    ck_assert_int_eq(mpz_cmp(curve_point.y, y), 0);
+
+    // Map curve point back to mpz_t
+    gmp_elligator_1_point_to_string(r, curve_point, curve);
+    ck_assert_int_eq(mpz_cmp(r, t), 0);
+
+    // Do the same with more complex inputs
+    mpz_set_str(t, "75BCD15", 16);
+    mpz_set_str(x,
+        "1A47D90E9F9C19AB846C9E100317F693607A1D16851FD05B40E7DA6FFF5BCF",
+        16);
+    mpz_set_str(y,
+        "5BBE4619CEA4729F94082B429693AC4B565F94CDA5D6D875689DE765C19A461",
+        16);
+
+    gmp_elligator_1_string_to_point(&curve_point, t, curve);
+    ck_assert_int_eq(mpz_cmp(curve_point.x, x), 0);
+    ck_assert_int_eq(mpz_cmp(curve_point.y, y), 0);
+
+    // Map curve point back to mpz_t
+    gmp_elligator_1_point_to_string(r, curve_point, curve);
+    ck_assert_int_eq(mpz_cmp(r, t), 0);
+
+    mpz_set_str(t,
+        "ABCDEF1234567899987654321ABCABCDEFDEF",
+        16);
+    mpz_set_str(x,
+        "7D4DD83C5E1F4E2DBC79CA0941102DE6376604C9D8A75EB41B6875010211B50",
+        16);
+    mpz_set_str(y,
+        "61C507D563235BEA439C1D53BDDF33E0D87C0041D3228CD0CA00C3D2A59025E",
+        16);
+
+    gmp_elligator_1_string_to_point(&curve_point, t, curve);
+    ck_assert_int_eq(mpz_cmp(curve_point.x, x), 0);
+    ck_assert_int_eq(mpz_cmp(curve_point.y, y), 0);
+
+    // Map curve point back to mpz_t
+    gmp_elligator_1_point_to_string(r, curve_point, curve);
+    ck_assert_int_eq(mpz_cmp(r, t), 0);
+
+    mpz_set_si(t, 2);
+    mpz_set_str(x, 
+        "6F5374156B145FF8BB3288E0418F513B5D7BBBAB6E252EA1BC2DB6428E1454E",
+        16);
+    mpz_set_str(y,
+        "ED7F6014F111318ED7F6014F111318ED7F6014F111318ED7F6014F111318EC",
+        16);
+
+    gmp_elligator_1_string_to_point(&curve_point, t, curve);
+    ck_assert_int_eq(mpz_cmp(curve_point.x, x), 0);
+    ck_assert_int_eq(mpz_cmp(curve_point.y, y), 0);
+
+    // Map curve point back to mpz_t
+    gmp_elligator_1_point_to_string(r, curve_point, curve);
+    ck_assert_int_eq(mpz_cmp(r, t), 0);
+
+    mpz_clear(t);
+    mpz_clear(x);
+    mpz_clear(y);
+    mpz_clear(r);
+
+    gmp_free_curve_point(&curve_point);
+    gmp_free_curve(&curve);
+}
+END_TEST
+
+/**
+ * \brief Test edge cases for Elligator mapping
+ */
+START_TEST(test_edge_cases)
+{
+    GMP_Curve curve;
+    GMP_CurvePoint curve_point;
+
+    mpz_t r, t, x, y;
+
+    mpz_init(curve_point.x);
+    mpz_init(curve_point.y);
+    mpz_init(r);
+    mpz_init(t);
+    mpz_init(x);
+    mpz_init(y);
+
+    // t = 1
+    gmp_init_curve1174(&curve);
+
+    mpz_set_si(t, 1);
+    mpz_set_si(x, 0);
+    mpz_set_si(y, 1);
+
+    gmp_elligator_1_string_to_point(&curve_point, t, curve);
+    ck_assert_int_eq(mpz_cmp(curve_point.x, x), 0);
+    ck_assert_int_eq(mpz_cmp(curve_point.y, y), 0);
+
+    gmp_elligator_1_point_to_string(r, curve_point, curve);
+    ck_assert_int_eq(mpz_cmp(r, t), 0);
+
+    mpz_clear(t);
+    mpz_clear(x);
+    mpz_clear(y);
+    mpz_clear(r);
+
+    gmp_free_curve_point(&curve_point);
+    gmp_free_curve(&curve);
+}
+END_TEST
+
+/**
+ * \brief Tests based on statements about curve1174 in paper
+ */
+START_TEST(test_advanced_curve1174)
+{
+    GMP_Curve curve;
+
+    mpz_t q_mod4, q_mod4_exp, temp1, temp2, inverse;
+
+    mpz_init(q_mod4);
+    mpz_init(q_mod4_exp);
+    mpz_init(temp1);
+    mpz_init(temp2);
+    mpz_init(inverse);
+
+    gmp_init_curve1174(&curve);
+
+    mpz_set_si(q_mod4_exp, 3);
+    mpz_set_si(q_mod4_exp, 3);
+    mpz_mod_ui(q_mod4, curve.q, 4);
+
+    ck_assert_int_eq(mpz_cmp(q_mod4, q_mod4_exp), 0);
+
+    // c * (c - 1) * (c + 1) != 0
+    mpz_set_si(temp1, 0);
+    mpz_set_si(temp2, 0);
+    ck_assert_int_ne(mpz_cmp_si(curve.c, 0), 0);
+
+    mpz_sub_ui(temp1, curve.c, 1);
+    ck_assert_int_ne(mpz_cmp_si(temp1, 0), 0);
+
+    mpz_add_ui(temp2, curve.c, 1);
+    ck_assert_int_ne(mpz_cmp_si(temp2, 0), 0);
+
+    // Check that d = -1174 mod q
+    mpz_set_si(temp1, -1174);
+    mpz_mod(temp1, temp1, curve.q);
+    ck_assert_int_eq(mpz_cmp(temp1, curve.d), 0);
+
+    // Check that d = -(c + 1)^2 / (c - 1)^2 mod q
+    mpz_set(temp1, curve.c);
+    mpz_add_ui(temp1, temp1, 1);
+    mpz_mul(temp1, temp1, temp1);
+    mpz_mod(temp1, temp1, curve.q);
+    mpz_neg(temp1, temp1);
+
+    mpz_set(temp2, curve.c);
+    mpz_sub_ui(temp2, temp2, 1);
+
+    mpz_mul(temp2, temp2, temp2);
+    mpz_mod(temp2, temp2, curve.q);
+    mpz_invert(inverse, temp2, curve.q);
+    mpz_mul(temp1, temp1, inverse);
+    mpz_mod(temp1, temp1, curve.q);
+    ck_assert_int_eq(mpz_cmp(temp1, curve.d), 0);
+
+    // r != 0
+    ck_assert_int_ne(mpz_cmp_si(curve.r, 0), 0);
+
+    // d is not square
+    mpz_chi(temp1, curve.d, curve.q);
+    ck_assert_int_eq(mpz_cmp_si(temp1, -1), 0);
+
+    mpz_clear(q_mod4);
+    mpz_clear(q_mod4_exp);
+    mpz_clear(temp1);
+    mpz_clear(temp2);
+    mpz_clear(inverse);
+
+    gmp_free_curve(&curve);
+}
+END_TEST
+
+/**
+ * \brief Tests based on statements about mapping in paper
+ */
+START_TEST(test_advanced_string_to_point)
+{
+    GMP_Curve curve;
+    GMP_CurvePoint curve_point;
+
+    mpz_t t, temp1, temp2, temp3;
+
+    mpz_init(curve_point.x);
+    mpz_init(curve_point.y);
+    mpz_init(t);
+    mpz_init(temp1);
+    mpz_init(temp2);
+    mpz_init(temp3);
+
+    gmp_init_curve1174(&curve);
+
+    mpz_set_str(t, "ABCDEF1234567899987654321ABCABCDEFDEF", 16);
+
+    gmp_elligator_1_string_to_point(&curve_point, t, curve);
+
+    // x^2 + y^2 = 1 + d * x^2 * y^2 (mod q)
+    mpz_mul(temp1, curve_point.x, curve_point.x);
+    mpz_mod(temp1, temp1, curve.q);
+    mpz_mul(temp2, curve_point.y, curve_point.y);
+    mpz_mod(temp2, temp2, curve.q);
+    mpz_mul(temp3, temp1, temp2);
+    mpz_mod(temp3, temp3, curve.q);
+    mpz_add(temp1, temp1, temp2);
+    mpz_mod(temp1, temp1, curve.q);
+    mpz_mul(temp3, curve.d, temp3);
+    mpz_mod(temp3, temp3, curve.q);
+    mpz_add_ui(temp3, temp3, 1);
+    mpz_mod(temp3, temp3, curve.q);
+
+    ck_assert_int_eq(mpz_cmp(temp1, temp3), 0);
+
+    // Same test with t = 1
+    mpz_set_si(t, 1);
+
+    gmp_elligator_1_string_to_point(&curve_point, t, curve);
+
+    mpz_mul(temp1, curve_point.x, curve_point.x);
+    mpz_mod(temp1, temp1, curve.q);
+    mpz_mul(temp2, curve_point.y, curve_point.y);
+    mpz_mod(temp2, temp2, curve.q);
+    mpz_mul(temp3, temp1, temp2);
+    mpz_mod(temp3, temp3, curve.q);
+    mpz_add(temp1, temp1, temp2);
+    mpz_mod(temp1, temp1, curve.q);
+    mpz_mul(temp3, curve.d, temp3);
+    mpz_mod(temp3, temp3, curve.q);
+    mpz_add_ui(temp3, temp3, 1);
+    mpz_mod(temp3, temp3, curve.q);
+
+    ck_assert_int_eq(mpz_cmp(temp1, temp3), 0);
+
+    mpz_clear(t);
+    mpz_clear(temp1);
+    mpz_clear(temp2);
+    mpz_clear(temp3);
+
+    gmp_free_curve(&curve);
+    gmp_free_curve_point(&curve_point);
 }
 END_TEST
 
 Suite *gmp_elligator_suite(void)
 {
     Suite *s;
-    TCase *tc_chi, *tc_basic;
+    TCase *tc_chi, *tc_basic, *tc_advanced;
 
     s = suite_create("GMP Elligator Test Suite");
 
@@ -264,10 +535,18 @@ Suite *gmp_elligator_suite(void)
     tcase_add_test(tc_chi, test_chi);
 
     tc_basic = tcase_create("Basic tests");
+
     tcase_add_test(tc_basic, test_curve1174);
+    tcase_add_test(tc_basic, test_e2e);
+    tcase_add_test(tc_basic, test_edge_cases);
+
+    tc_advanced = tcase_create("Advanced Tests");
+    tcase_add_test(tc_advanced, test_advanced_curve1174);
+    tcase_add_test(tc_advanced, test_advanced_string_to_point);
 
     suite_add_tcase(s, tc_chi);
     suite_add_tcase(s, tc_basic);
+    suite_add_tcase(s, tc_advanced);
 
     return s;
 }
